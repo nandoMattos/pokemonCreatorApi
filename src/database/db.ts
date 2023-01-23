c3f56b5daae3ef7613af1b37cfc4f12a6e84979a
Luiz Fernando Mattos <luizfernandoamattos@gmail.com>
2023-01-23 15:31:22 -0300

    config: add .env.example

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,13 +1,11 @@
 import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
 
 const { Pool } = pg;
 
 const connection = new Pool({
-  host: "localhost",
-  port: 5432,
-  user: "postgres",
-  password: "postgres",
-  database: "pokemon",
+  connectionString: process.env.CONNECTION_STRING,
 });
 
 export default connection;
